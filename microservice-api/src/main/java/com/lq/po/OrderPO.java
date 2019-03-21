3ed73ceaddbee01a35d0a28bea986af6747c923b
lqyhcn <liqianjava@163.com>
2019-03-21 23:37:21 +0800

    新增OrderPO.java

---
+++ microservice-api/src/main/java/com/lq/po/OrderPO.java
@@ -0,0 +1,14 @@
+package com.lq.po;
+
+/**
+ * 鸟欲高飞先振翅
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: liqian
+ * @Date: 2019-03-21
+ * @Time: 23:36
+ * 人求上进先读书
+ */
+public class OrderPO {
+}
